dfc5a2eefe3bb965a60fe3bde0df72b2550a00b1
hansol0118 <wall197944@naver.com>
2026-04-30 11:07:34 +0900

    step 1

--- src/main/kotlin/wiseSaying/Main.kt
+++ src/main/kotlin/wiseSaying/Main.kt
@@ -1,16 +1,18 @@
 package wiseSaying
 
-fun main(){
+fun main() {
 
     println("== 명언 앱 ==")
 
-    while(true){
+    while (true) {
         print("명령) ")
-
         val input = readln()
-        when(input == "종료"){
-            println("프로그램을 종료합니다")
-            break
+
+        when (input) {
+            "종료" -> {
+                println("프로그램을 종료합니다")
+                break
+            }
         }
     }
 }
